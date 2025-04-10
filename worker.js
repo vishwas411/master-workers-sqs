18c7c7186f63ed6aeb8410cc03ccab9823f03d77
VishwasSahu <vishwas.sahu@celigo.com>
2025-04-10 14:30:16 +0530

    enabled pooling in consumers

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
-const { MongoClient } = require('mongodb')
 const { fork } = require('child_process')
+const { MongoClient, ObjectId } = require('mongodb')
 const path = require('path')
 const nconf = require('nconf')
 
@@ -9,7 +9,10 @@ const MONGO_URI = nconf.get('MONGODB_URI')
 const DB_NAME = nconf.get('MONGODB_NAME')
 const COLLECTION_NAME = 'queues'
 
-const activeConsumers = new Map() // key = queue._id.toString(), value = child process
+const MAX_CONSUMERS = 5
+const consumers = [] // array of child processes
+const idleConsumers = new Set() // consumer index
+const activeAssignments = new Map() // queueId -> consumer index
 
 async function startWorkerManager() {
   const client = new MongoClient(MONGO_URI)
@@ -17,37 +20,71 @@ async function startWorkerManager() {
   const db = client.db(DB_NAME)
   const collection = db.collection(COLLECTION_NAME)
 
-  console.log('Worker Manager started. Polling DB for queue assignments...')
+  console.log('Worker Manager started. Polling DB for queues...')
 
   async function pollDB() {
     try {
       const queues = await collection.find({}).toArray()
 
-      for (const q of queues) {
-        const queueId = q._id.toString()
+      for (const queue of queues) {
+        const queueId = queue._id.toString()
 
-        if (!activeConsumers.has(queueId)) {
-          const queueUrl = q.queueUrl
-          console.log(`Forking consumer for queue ID: ${queueId}, URL: ${queueUrl}`)
+        // If this queue is not already being processed
+        if (!activeAssignments.has(queueId)) {
+          // Reuse idle consumer if available
+          if (idleConsumers.size > 0) {
+            const consumerIndex = Array.from(idleConsumers)[0]
+            idleConsumers.delete(consumerIndex)
+            activeAssignments.set(queueId, consumerIndex)
 
-          const consumer = fork(path.join(__dirname, 'consumer.js'), [], {
-            env: {
-              ...process.env,
-              QUEUE_ID: queueId,
-              QUEUE_URL: queueUrl
-            }
-          })
+            consumers[consumerIndex].send({
+              type: 'assign',
+              queueId,
+              queueUrl: queue.queueUrl,
+              consumerIndex
+            })
 
-          activeConsumers.set(queueId, consumer)
+            console.log(`Assigned queue ${queueId} to consumer ${consumerIndex}`)
+          } else if (consumers.length < MAX_CONSUMERS) {
+            // Create a new consumer if under the max limit
+            const consumerIndex = consumers.length
+            const consumer = fork(path.join(__dirname, 'consumer.js'))
 
-          consumer.on('exit', code => {
-            console.log(`Consumer for ID ${queueId} exited with code ${code}`)
-            activeConsumers.delete(queueId)
-          })
+            consumers.push(consumer)
+            activeAssignments.set(queueId, consumerIndex)
 
-          consumer.on('message', msg => {
-            console.log(`Consumer [${queueId}] message:`, msg)
-          })
+            consumer.on('message', async msg => {
+              if (msg.type === 'done') {
+                const donePid = msg.consumerPid
+                const doneQueueId = msg.queueId
+            
+                console.log(`Consumer PID ${donePid} finished queue ${doneQueueId}`)
+                activeAssignments.delete(doneQueueId)
+                idleConsumers.add(consumers.findIndex(c => c.pid === donePid))
+            
+                try {
+                  await collection.deleteOne({ _id: new ObjectId(doneQueueId) })
+                  console.log(`Deleted queue ${doneQueueId} from DB`)
+                } catch (err) {
+                  console.error(`Failed to delete queue ${doneQueueId}:`, err)
+                }
+              }
+            })
+
+            consumer.on('exit', () => {
+              console.log(`Consumer ${consumerIndex} exited`)
+              idleConsumers.delete(consumerIndex)
+            })
+
+            consumer.send({
+              type: 'assign',
+              queueId,
+              queueUrl: queue.queueUrl,
+              consumerIndex
+            })
+
+            console.log(`Assigned queue ${queueId} to consumer PID ${consumers[consumerIndex].pid}`)
+          }
         }
       }
     } catch (err) {
@@ -61,6 +98,6 @@ async function startWorkerManager() {
 }
 
 startWorkerManager().catch(err => {
-  console.error('Failed to start worker manager:', err)
+  console.error('Worker manager failed to start:', err)
   process.exit(1)
 })
